e7065279e5098920bb9a784f1bf53a0127186fa0
zgwyvd11 <wangli3703@163.com>
2017-05-31 16:55:06 +0800

    first project

---
+++ ScrollItem/ScrollItem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollItem
+//
+//  Created by joint on 17/5/31.
+//  Copyright © 2017年 joint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollItem/ScrollItem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollItem
+//
+//  Created by joint on 17/5/31.
+//  Copyright © 2017年 joint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
